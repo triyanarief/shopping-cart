d05a4428193e52e9555ae6225bf8a4aaa7105668
triyanarief <triyanarief@icloud.com>
2017-04-17 10:12:29 +0700

    adding mongoDB mongoose

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var expressHbs = require('express-handlebars');
+var mongoose = require('mongoose');
 
 var index = require('./routes/index');
 
 var app = express();
 
+mongoose.connect('localhost:27017/shopcart');
 // view engine setup
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
 app.set('view engine', '.hbs');
