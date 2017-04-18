1a57fe62791c0923ff88640f74a13a4833dbb80d
triyanarief <triyanarief@icloud.com>
2017-04-18 11:26:54 +0700

    signup, signin, logout done passport

--- app.js
+++ app.js
@@ -9,12 +9,16 @@ var mongoose = require('mongoose');
 var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
+var validator = require('express-validator');
 
-var index = require('./routes/index');
+var routes = require('./routes/index');
+var userRoutes = require('./routes/user');
 
 var app = express();
 
 mongoose.connect('localhost:27017/shopcart');
+require('./config/passport');
+
 // view engine setup
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
 app.set('view engine', '.hbs');
@@ -24,6 +28,7 @@ app.set('view engine', '.hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
 app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
 app.use(flash());
@@ -31,7 +36,13 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use(function(req, res, next) {
+    res.locals.login = req.isAuthenticated();
+    next();
+});
+
+app.use('/user', userRoutes);
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,15 +51,29 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handlers
 
-  // render the error page
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,18 @@
 var express = require('express');
 var router = express.Router();
-var Product = require('../models/product');
-var csrf = require('csurf');
 
-var csrfProtection = csrf();
-router.use(csrfProtection);
+var Product = require('../models/product');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-Product.find(function(err, docs) {
-    var productChunks = [];
-    var chunkSize = 3;
-    for (var i = 0; i < docs.length; i += chunkSize) {
-      productChunks.push(docs.slice(i, i + chunkSize));
-    }
-    res.render('shop/index', { title: 'Shopping Cart', products: productChunks});
-  });
+router.get('/', function (req, res, next) {
+    Product.find(function (err, docs) {
+        var productChunks = [];
+        var chunkSize = 3;
+        for (var i = 0; i < docs.length; i += chunkSize) {
+            productChunks.push(docs.slice(i, i + chunkSize));
+        }
+        res.render('shop/index', {title: 'Shopping Cart', products: productChunks});
+    });
 });
-
-router.get('/user/signup', function(req, res, next) {
-  res.render('user/signup', {csrfToken: req.csrfToken()});
-})
-
-router.post('/user/signup', function(req, res, next) {
-  res.redirect('/');
-})
 
 module.exports = router;
