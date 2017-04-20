27443a22a59c28a19b945fc2bb45d912f4929137
triyanarief <triyanarief@icloud.com>
2017-04-20 10:34:42 +0700

    cart model

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
 var Product = require('../models/product');
+var Cart = require('../models/cart');
 
-/* GET home page. */
+router.use(function (req, res, next) {
+    res.locals.login = req.isAuthenticated();
+    next();
+});
+
 router.get('/', function (req, res, next) {
     Product.find(function (err, docs) {
         var productChunks = [];
@@ -13,6 +17,25 @@ router.get('/', function (req, res, next) {
         }
         res.render('shop/index', {title: 'Shopping Cart', products: productChunks});
     });
+});
+
+router.get('/add-to-cart/:id', function (req, res, next) {
+    var productId = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart.items : {});
+
+    Product.findById(productId, function (err, product) {
+        cart.add(product, product.id);
+        req.session.cart = cart;
+        res.redirect('/');
+    });
+});
+
+router.get('/shopping-cart', function (req, res, next) {
+    if (!req.session.cart) {
+        return res.render('shop/shopping-cart', {products: null});
+    }
+    var cart = new Cart(req.session.cart.items);
+    res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
 });
 
 module.exports = router;
